ffedd600e399f20debf9c9339e218b15706a5834
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-03-06 06:51:06 +0500

    Displaying game critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "@chakra-ui/react";
+
+function CriticScore({ score }: { score: number }) {
+  let color: string;
+  if (score > 75) color = "green";
+  else if (score > 60) color = "yellow";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius="2px"
+    >
+      {score}
+    </Badge>
+  );
+}
+
+export default CriticScore;
