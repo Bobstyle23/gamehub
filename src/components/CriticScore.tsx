9b39093c7280a352863500346ed6017a992e9db7
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-03-06 07:09:47 +0500

    Get optimized images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,9 +1,10 @@
 import { Badge } from "@chakra-ui/react";
 
 function CriticScore({ score }: { score: number }) {
-  let color: string;
+  let color: string = "";
   if (score > 75) color = "green";
   else if (score > 60) color = "yellow";
+
   return (
     <Badge
       colorScheme={color}
