24adc23c27c5481b3ac699fe3fa4dbcb5c67254c
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-03-07 23:29:48 +0500

    Sort games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 function getCroppedImageUrl(url: string): string {
+  if (!url) return "No Image";
   const TARGET = "media/";
   const index = url.indexOf(TARGET) + TARGET.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
