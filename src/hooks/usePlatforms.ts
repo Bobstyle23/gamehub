226b6be0ee0cbe2f97b8fdaf54c8aeeb3e0f4340
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-03-10 00:14:57 +0500

    Fetch platforms with React-Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Platform {
   id: number;
@@ -7,7 +10,16 @@ export interface Platform {
 }
 
 function usePlatforms() {
-  return { data: platforms, isLoading: false, error: null };
+  const { data, isLoading, error } = useQuery<Platform[], Error>({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/parent_platforms")
+        .then((res) => res.data.results),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: platforms,
+  });
+  return { data, isLoading, error };
 }
 
 export default usePlatforms;
