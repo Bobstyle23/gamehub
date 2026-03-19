ca8f032193d002ec6407fe4ba8e47f9a72382353
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-03-20 00:40:25 +0500

    Remove deleted file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -17,7 +16,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-        {/*<App />*/}
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
