5a17115d89db76976752d5601c162c32a549d6f1
Mukhammad Bobur Pakhriev <boburpakhriev@gmail.com>
2026-03-23 19:49:25 +0500

    Fix the NavBar search issue

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,20 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../stores/gameQueryStore";
+import { useNavigate } from "react-router-dom";
 
 function SearchInput() {
   const setSearchValue = useGameQueryStore((store) => store.setSearchValue);
   const searchRef = useRef<HTMLInputElement>(null);
+
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (searchRef.current) setSearchValue(searchRef.current.value);
+        navigate("/");
       }}
     >
       <InputGroup>
